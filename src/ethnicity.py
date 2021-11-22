2c04cd0e64e8f6ae9612d54e9266280829b9e461
陳新元 <b05901171@ntu.edu.tw>
2021-11-22 13:50:21 -0800

    update

--- src/ethnicity.py
+++ src/ethnicity.py
@@ -7,9 +7,57 @@ import cleanPopulation
 import cleanDeath
 
 
-def 
+def plotPie(data, title):
+    """
+    Using
+    """
+    plt.pie(data['Count'], autopct='%1.1f%%', labels=data['Cause_Desc'])
+    plt.title(title)
+    plt.rc('font', size=16)
+    plt.show()
+    # if '/' in title:
+    #     strr = ""
+    #     for i in title.split('/'):
+    #         strr += i+' '
+    #     plt.savefig(strr[:-1]+'.png')
+    # else:
+    #     plt.savefig(title+'.png')
+    # plt.close()
 
-def cleanEthniciyData():
+
+def cleanEthnicityDataByCounty(csvFile='../data/death.csv', plot = False, county='San Diego'):
+    """
+    Clean race data for specific county
+    """
+    assert isinstance(csvFile, str) and os.path.exists(csvFile), 'Invalid path'
+    df = pd.read_csv(csvFile)
+    data = df.groupby(['County']).get_group(county)
+    data = data.groupby('Strata').get_group('Race-Ethnicity')
+    data = data.set_index('Strata_Name')
+    data = data.drop(columns=['Annotation_Code', 'Year', 'Geography_Type', 'Cause', 'Annotation_Desc', 'Strata'])
+    counting = data.groupby(['Strata_Name']).sum().reset_index()
+    counting.Count = counting.Count.astype(int)
+    #data = data.drop(columns=['Strata', 'Unnamed: 0', 'Year'])
+    
+    if plot:
+        temp = counting.iloc[-1].copy()
+        counting.iloc[-1] = counting.iloc[-2]
+        counting.iloc[-2] = temp
+        temp = counting.iloc[0].copy()
+        counting.iloc[0] = counting.iloc[1]
+        counting.iloc[1] = temp
+        print(counting)
+        plt.rc('font', size=16)
+        plt.pie(counting['Count'], autopct='%1.1f%%', labels=counting['Strata_Name'])
+        plt.show()
+    data = data.groupby(['Strata_Name', 'Cause_Desc']).sum().reset_index()
+    data.Count = data.Count.astype(int)
+
+    #Plot causes pie chart
+    if plot:
+        for name, group in data.groupby('Strata_Name'):
+            plotPie(group.reset_index().drop([1]), name)
+def cleanEthniciyData(plot = False):
     """
     Plot death according to age
     """
@@ -17,35 +65,29 @@ def cleanEthniciyData():
     data = data.groupby('Strata').get_group('Race-Ethnicity')
     data = data.set_index('Strata_Name')
     data = data.drop(columns=['Strata', 'Unnamed: 0', 'Year'])
+
+    # Plot death race composition
+    counting = data.groupby(['Cause_Desc']).sum()
+    counting = data.groupby(['Strata_Name']).sum().reset_index()
+    if plot:
+        temp = counting.iloc[-1].copy()
+        counting.iloc[-1] = counting.iloc[-2]
+        counting.iloc[-2] = temp
+        temp = counting.iloc[0].copy()
+        counting.iloc[0] = counting.iloc[1]
+        counting.iloc[1] = temp
+        print(counting)
+        plt.rc('font', size=16)
+        plt.pie(counting['Count'], autopct='%1.1f%%', labels=counting['Strata_Name'])
+        plt.show()
     data = data.groupby(['Strata_Name', 'Cause_Desc']).sum().reset_index()
-    for i in data.groupby('Strata_Name'):
-        print(i)
-    print(data)
-    # data = data.groupby('Strata').get_group('Age')
-    # data = data.set_index('Strata_Name')
-    # data = data.drop(columns=['Strata', 'Cause_Desc', 'Unnamed: 0'])
-    # changeName = {'1-4 years': 'Under 5 years', 'Under 1 year': 'Under 5 years', '15-24 years': '5-24 years', '5-14 years': '5-24 years', '75-84 years': '75 years and over', '85 years and over': '75 years and over'}
-    # dicts = {}
-    # for index, row in data.iterrows():
-    #     if index in changeName:
-    #         index = changeName[index]
-    #     if index not in dicts:
-    #         dicts[index] = defaultdict(int)
-    #     dicts[index][row[0]] += row[1]
-    # print(data)
-    # def plotPieData(dicts, year=2019, normalize = False): 
-    #     if not normalize:
-    #         plt.pie([dicts[i][year] for i in dicts], labels=[i for i in dicts], autopct='%1.1f%%')
-    #         plt.title(str(year)+' Non-normalized age pie chart')
-    #         plt.show()
-    #     else:
-    #         data = cleanPopulation.cleanAgeData()
-    #         plt.pie([dicts[i][year]*100/data[i][str(year)] for i in dicts], labels=[i for i in dicts], autopct='%1.1f%%')
-    #         plt.title(str(year)+' Normalized age pie chart')
-    #         plt.legend()
-    #         plt.show()
-    # plotPieData(dicts)
-    # plotPieData(dicts, normalize=True)
+
+    #Plot causes pie chart
+    if plot:
+        for name, group in data.groupby('Strata_Name'):
+            plotPie(group, name)
+
 
 if __name__ == '__main__':
-    cleanEthniciyData()
+    cleanEthniciyData(plot = True)
+    cleanEthnicityDataByCounty(plot = True)
