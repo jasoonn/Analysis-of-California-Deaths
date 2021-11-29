1127fac8d47894d4705d96e2870e057f8714bed9
陳新元 <b05901171@ntu.edu.tw>
2021-11-28 20:45:24 -0800

    update

--- src/ethnicity.py
+++ src/ethnicity.py
@@ -7,7 +7,7 @@ import cleanPopulation
 import cleanDeath
 
 
-def plotPiee(data, label, title=None, fontSize=16):
+def plotPiee(data, label, title=None, fontSize=13):
     """Plot piechart from given data and label
     :param: data, label, title
     :type: list, list, str
@@ -67,6 +67,84 @@ def cleanEthnicityDataByCounty(csvFile='../data/death.csv', plot = False, county
             plotPie(group.reset_index().drop([1]), name)
 
 
+def includeCounty(csvFile='../data/death.csv', outputFile='../data/deathCombine.csv'):
+    """
+    Read death dataset and return county-combined data
+    csv_file: path to csv file (dataset)
+    :return: Dataframe
+    """
+    assert isinstance(csvFile, str) and os.path.exists(csvFile), 'Invalid path'
+    df = pd.read_csv(csvFile)
+    grp = df.groupby(['Year', 'Strata', 'Strata_Name', 'Cause_Desc'])
+    data = grp.sum()
+    data = data.iloc[:, :-1]
+    data = data.reset_index()
+    data.Count = data.Count.astype(int)
+    return data
+
+def filterEthnicityDataByCounty(csvFile='../data/death.csv', plot = False, county='San Diego'):
+    """
+    Clean race data for specific county
+    """
+    assert isinstance(csvFile, str) and os.path.exists(csvFile), 'Invalid path'
+    df = pd.read_csv(csvFile)
+    data = df.groupby(['County']).get_group(county)
+    data = data.groupby('Strata').get_group('Race-Ethnicity')
+    data = data.set_index('Strata_Name')
+    data = data.drop(columns=['Annotation_Code', 'Year', 'Geography_Type', 'Cause', 'Annotation_Desc', 'Strata'])
+    data = data.groupby(['Strata_Name', 'Cause_Desc']).sum().reset_index()
+    data.Count = data.Count.astype(int)
+    considerGroup = ['Black', 'White', 'Hispanic', 'Asian']
+    consider = ['Assault (homicide)', 'Alzheimer\'s disease', 'Malignant neoplasms', 'Intentional self-harm (suicide)', 'Diseases of heart']
+
+    #Plot causes pie chart
+    if plot:
+        tmp = [0]*4
+        fig, ((tmp[0], tmp[1]), (tmp[2], tmp[3])) = plt.subplots(2, 2)
+        plt.rcParams["font.family"] = "Times New Roman"
+        plt.rc('font', size=13)
+        count = 0
+        for name, group in data.groupby('Strata_Name'):
+            if name in considerGroup:
+                allNum = group.set_index('Cause_Desc')['Count']['All causes (total)']
+                group = group[group['Cause_Desc'].isin(consider)]
+                group = group.append({'Strata_Name': name, 'Cause_Desc': 'Other', 'Count': allNum-group['Count'].sum()} , ignore_index=True)
+                tmp[count].pie(group['Count'], autopct='%1.1f%%', labels=group['Cause_Desc'])
+                tmp[count].set_title(name, fontdict = {"fontfamily": "Times New Roman"})
+                count += 1
+        fig.suptitle('San Diego', size=20)
+        plt.show()
+
+
+def filterEthniciyData(plot = False):
+    """
+    Plot death according to age
+    """
+    data = includeCounty()
+    data = data.groupby('Strata').get_group('Race-Ethnicity')
+    data = data.set_index('Strata_Name')
+    data = data.drop(columns=['Strata', 'Year'])
+    #Plot causes pie chart
+    data = data.groupby(['Strata_Name', 'Cause_Desc']).sum().reset_index()
+    considerGroup = ['Black', 'White', 'Hispanic', 'Asian']
+    consider = ['Assault (homicide)', 'Alzheimer\'s disease', 'Malignant neoplasms', 'Intentional self-harm (suicide)', 'Diseases of heart']
+    if plot:
+        tmp = [0]*4
+        fig, ((tmp[0], tmp[1]), (tmp[2], tmp[3])) = plt.subplots(2, 2)
+        plt.rcParams["font.family"] = "Times New Roman"
+        plt.rc('font', size=13)
+        count = 0
+        for name, group in data.groupby('Strata_Name'):
+            if name in considerGroup:
+                allNum = group.set_index('Cause_Desc')['Count']['All causes (total)']
+                group = group[group['Cause_Desc'].isin(consider)]
+                group = group.append({'Strata_Name': name, 'Cause_Desc': 'Other', 'Count': allNum-group['Count'].sum()} , ignore_index=True)
+                tmp[count].pie(group['Count'], autopct='%1.1f%%', labels=group['Cause_Desc'])
+                tmp[count].set_title(name, fontdict = {"fontfamily": "Times New Roman"})
+                count += 1
+        fig.suptitle('California', size=20)
+        plt.show()
+
 def cleanEthniciyData(plot = False):
     """
     Plot death according to age
@@ -82,7 +160,6 @@ def cleanEthniciyData(plot = False):
     if plot:
         swtichColumn(counting, -1, -2)
         swtichColumn(counting, 0, 1)
-        print(counting)
         plt.rc('font', size=16)
         plt.pie(counting['Count'], autopct='%1.1f%%', labels=counting['Strata_Name'])
         plt.show()
@@ -95,5 +172,7 @@ def cleanEthniciyData(plot = False):
             
 
 if __name__ == '__main__':
-    cleanEthniciyData(plot = True)
-    cleanEthnicityDataByCounty(plot = True)
+    cleanEthniciyData(plot = False)
+    cleanEthnicityDataByCounty(plot = False)
+    filterEthniciyData(plot = False)
+    filterEthnicityDataByCounty(plot = True)
