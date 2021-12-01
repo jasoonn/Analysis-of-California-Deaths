919d0032b0874a087d8112c9c1c6d06a0d774b73
Samveed <desaisamveed@gmail.com>
2021-12-01 15:03:43 -0800

    Updated influenza related graph

--- src/get_county_stats.py
+++ src/get_county_stats.py
@@ -78,6 +78,10 @@ def get_county_deaths_by_disease():
         break
                 
 def car_accidents_percentage_increase():
+    '''
+    Resource:
+    https://ktla.com/news/local-news/traffic-related-fatalities-increased-in-2019-as-number-of-collisions-dropped-in-l-a-police-chief-says/
+    '''
     car_accidents_column = 'Accidents (unintentional injuries)'
     accidents_df = year_county_deaths[[COUNTY_COL, YEAR_COL, car_accidents_column]]
     concerned_years = [2018,2019]
@@ -89,8 +93,7 @@ def car_accidents_percentage_increase():
         accident_value = last_2_years_accidents.loc[last_2_years_accidents[COUNTY_COL] == county]
         value_2018 = (accident_value[car_accidents_column].iloc[0])
         value_2019 = (accident_value[car_accidents_column].iloc[len(accident_value.index)-1])
-        percentage_increase[county] = ((value_2019-value_2018)/value_2018)*100;
-        
+        percentage_increase[county] = ((value_2019-value_2018)/value_2018)*100;  
     
     total_items = len(percentage_increase)-1
     total_increase_other_counties = 0
@@ -109,12 +112,55 @@ def car_accidents_percentage_increase():
     plt.ylabel("Percentage increase in deaths due to accidents")
     plt.title(" % Increase in deaths due to accidents from 2018 to 2019")
     
+    plt.show()
+    # plt.savefig(f'{VISUALIZATION_DIR}/accident/accident_analysis.png')
     
-    plt.savefig(f'{VISUALIZATION_DIR}/accident/accident_analysis.png')
+def get_homicide_rates_county():
+    homicide_column = 'Assault (homicide)'
+    concerned_county = 'San Diego'
+    homicide_df = year_county_deaths[[COUNTY_COL, YEAR_COL, homicide_column]]
+    print(homicide_df)
+    concerned_years = [2017,2018,2019]
+    san_diego_homicides = homicide_df.loc[homicide_df[COUNTY_COL] == concerned_county]
+    san_diego_homicides = san_diego_homicides.loc[san_diego_homicides[YEAR_COL].isin(concerned_years)]
     
+    # print(san_diego_homicides.iloc[0])
     
+def get_influenza_rates_county():
+    influenza_col = 'Influenza and pneumonia'
+    influenza_df = year_county_deaths[[COUNTY_COL, YEAR_COL, influenza_col]]
+    concerned_years = [2018, 2019]
+    county_influenza_df = influenza_df.loc[influenza_df[COUNTY_COL].isin(county_names)]
+    county_influenza_df = county_influenza_df.loc[county_influenza_df[YEAR_COL].isin(concerned_years)]
+    county_influenza_df.sort_values(by=[YEAR_COL])
+    # print(county_influenza_df[county_influenza_df[YEAR_COL] == 2018])
+    # values_2018 = county_influenza_df.loc[county_influenza_df[YEAR_COL] == '2018']
+    # print(values_2018)
+    values = defaultdict(list)
+    values_year_1 = list()
+    values_year_2 = list()
+    for year in (concerned_years):
+        for county in county_names:
+            values[county].append((county_influenza_df[county_influenza_df[YEAR_COL] == year][county_influenza_df[COUNTY_COL] == county][influenza_col].iloc[0]))
+    for _,value in values.items():
+        values_year_1.append(value[0])
+        values_year_2.append(value[1])
+    
+    n = len(county_names)
+    r = np.arange(n)
+    plt.figure()
+    width = 0.2
+    plt.bar(r, values_year_1, width, label = '2018')
+    plt.bar(r + 0.2, values_year_2, width, label = '2019')
+    plt.xlabel("Counties")
+    plt.ylabel("Number of deaths per year due to influenza")
+    plt.title(" Trend of deaths due to influenza from 2018 to 2019")
+    plt.xticks(r,county_names,rotation=25,fontsize=7)
+    plt.legend()
+    plt.savefig(f'{VISUALIZATION_DIR}/accident/influenza.png')
         
-    
-car_accidents_percentage_increase()  
+# get_homicide_rates_county()
+# get_influenza_rates_county()
+# car_accidents_percentage_increase()  
 # disease_county_dist_yearly_analysis(year_county_deaths, 2014)
 # multiple_bar_charts_per_disease(year_county_deaths)
