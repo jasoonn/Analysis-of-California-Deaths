311a149d4da61c3dd23d5fc61c4c3d7121845699
陳新元 <b05901171@ntu.edu.tw>
2021-12-02 06:40:04 -0800

    update util and ethnicity

--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def plotPie(data, label, title=None, fontSize=16):
     """Plot piechart from given data and label
@@ -11,6 +12,16 @@ def plotPie(data, label, title=None, fontSize=16):
     plt.title(title)
     plt.pie(data, autopct='%1.1f%%', labels=label)
     plt.show()
+
+def swtichColumn(data, i ,j):
+    """Switch two specific columns in pandas
+    :param: data, i, j
+    :type: DataFrame, int, int
+    :returns: None
+    """
+    temp = data.iloc[i].copy()
+    data.iloc[i] = data.iloc[j]
+    data.iloc[j] = temp
 
 if __name__ == '__main__':
     vals = [2, 4, 6]
